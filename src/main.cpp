0b868c5e2481bb7761e2aabcfaf174755397e044
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-10-25 21:50:29 +0100

    another fix for begin anchor backtracking

--- src/main.cpp
+++ src/main.cpp
@@ -479,11 +479,13 @@ bool matcher_internal(
       return matcher_internal(
         input, input_pos, pattern, pattern_pos + 1, anchors);
     }
-    if ((anchors & anchor_e::begin) != 1) {
+    if (
+      (anchors & anchor_e::begin) == 0
+      || (anchors & anchor_e::begin) == 1 && input_pos != 0) {
       return matcher_internal(
         input, input_pos + 1, pattern, pattern_pos, anchors);
     } else {
-      return input_pos != 0;
+      return false;
     }
   }
 }
@@ -513,12 +515,13 @@ int main(int argc, char* argv[]) {
     // auto p = parse_pattern(std::string("a\\d+"));
     // auto p = parse_pattern(std::string("a[123]+123"));
     // auto p = parse_pattern(std::string("a123$"));
-    auto p = parse_pattern(std::string("^abc"));
+    // auto p = parse_pattern(std::string("^abc"));
     // auto p = parse_pattern(std::string("^[jmav]+"));
     // auto p = parse_pattern(std::string("this$"));
     // auto p = parse_pattern(std::string("ca+aars"));
     // auto p = parse_pattern(std::string("d"));
-    // auto p = parse_pattern(std::string("^strawberry$"));
+    auto p = parse_pattern(std::string("^strawberry$"));
+    // auto p = parse_pattern(std::string("^abc_\\d+_xyz$"));
     // auto p = parse_pattern(std::string("^abc_\\d+_xyz$"));
 
     bool test = false;
@@ -526,13 +529,14 @@ int main(int argc, char* argv[]) {
     // auto input = std::string("aaaxbbbacy");
     // auto input = std::string("helloa123");
     // auto input = std::string("a123123123123");
-    auto input = std::string("abcthisisabc");
+    // auto input = std::string("abcthisisabc");
     // auto input = std::string("thisisajvm");
     // auto input = std::string("thisisnotthis");
     // auto input = std::string("caaars");
     // auto input = std::string("dog");
-    // auto input = std::string("strawberry");
+    auto input = std::string("strawberry");
     // auto input = std::string("abc_123_xyz");
+    // auto input = std::string("abc_rst_xyz");
     // for (int i = 0; i < input.size(); i++) {
     // move starting position forward
     test = matcher(input, p);
